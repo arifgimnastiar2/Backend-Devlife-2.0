be1ab7ccb82ae63b61c0803ce84f2bac5291a509
Rafzy <khoirulloh.rafi2@gmail.com>
2021-11-06 14:21:53 +0700

    Initialize Laravel Breeze

--- resources/views/components/navigation/sidebar.blade.php
+++ resources/views/components/navigation/sidebar.blade.php
@@ -8,12 +8,8 @@
           </div>
           <ul class="sidebar-menu">
               <li class="menu-header">Dashboard</li>
-              <li class="nav-item dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                  <li class="active"><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-                </ul>
+              <li class="nav-item active">
+                <a href="#" class="nav-link "><i class="fas fa-fire"></i><span>Dashboard</span></a>
               </li>
               <li class="menu-header">Starter</li>
               <li class="nav-item dropdown">
