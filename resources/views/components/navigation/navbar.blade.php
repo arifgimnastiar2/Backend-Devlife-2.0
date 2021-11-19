fe3a9526c321e7ecc17f8697836213a755c17cac
Rafzy <khoirulloh.rafi2@gmail.com>
2021-11-19 19:07:47 +0700

    Slicing Design Login v2

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -194,7 +194,8 @@
       </li>
       <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
         <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
-        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+        @auth
+        <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div></a>
         <div class="dropdown-menu dropdown-menu-right">
           <div class="dropdown-title">Logged in 5 min ago</div>
           <a href="features-profile.html" class="dropdown-item has-icon">
@@ -207,10 +208,40 @@
             <i class="fas fa-cog"></i> Settings
           </a>
           <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item has-icon text-danger">
+          <a href="#" class="dropdown-item has-icon text-danger" onclick="event.preventDefault(); 
+            document.getElementById('form-submit').submit()">
             <i class="fas fa-sign-out-alt"></i> Logout
+
+            <form action="{{ route('logout') }}" id="form-submit" method="POST" class="d-none">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+            </form>
+          </a>
+        </div>  
+        @else
+          <div class="d-sm-none d-lg-inline-block">Hi, Unknown</div></a>
+        <div class="dropdown-menu dropdown-menu-right">
+          <div class="dropdown-title">Logged in 5 min ago</div>
+          <a href="features-profile.html" class="dropdown-item has-icon">
+            <i class="far fa-user"></i> Profile
+          </a>
+          <a href="features-activities.html" class="dropdown-item has-icon">
+            <i class="fas fa-bolt"></i> Activities
+          </a>
+          <a href="features-settings.html" class="dropdown-item has-icon">
+            <i class="fas fa-cog"></i> Settings
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item has-icon text-danger" onclick="event.preventDefault(); 
+            document.getElementById('form-submit').submit()">
+            <i class="fas fa-sign-out-alt"></i> Logout
+
+            <form action="{{ route('logout') }}" id="form-submit" method="POST" class="d-none">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+            </form>
           </a>
         </div>
+        @endauth
+        
       </li>
     </ul>
   </nav>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+  return view('welcome');
+})->name('home');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
