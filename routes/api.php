30dc6750ecfa56ca059e946ada2409a3dbcca379
Arif Gimnastiar <arif.gimnastiar2004@gmail.com>
2021-12-23 16:13:46 +0700

    API Authentication with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//API route for register new user
+Route::post('/register', [UserController::class, 'register']);
+//API route for login user
+Route::post('/login', [UserController::class, 'login']);
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    // API route for logout user
+    Route::post('/logout', [UserController::class, 'logout']);
 });
