96e14327e35ce3e071ef7aacb9f028dc9292cf47
naufalakmal <muhammadnaufalakmal1@gmail.com>
2022-01-26 20:26:26 +0700

    Sidebar fix

--- resources/views/components/argon/navigation/Sidebar.blade.php
+++ resources/views/components/argon/navigation/Sidebar.blade.php
@@ -2,9 +2,10 @@
   <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
     <div class="scrollbar-inner">
       <!-- Brand -->
-      <div class="sidenav-header  align-items-center">
+      <div class="sidenav-header align-items-center">
         <a class="navbar-brand" href="javascript:void(0)">
-          <img src="{{ asset('img/app-logo.png') }}" class="navbar-brand-img" alt="app-logo">
+          <img src="{{ asset('img/logo.png') }}" class="navbar-brand-img" alt="app-logo">
+          <h2 class="d-inline-block ml-1 mt-4">Devector</h2>
         </a>
       </div>
       <div class="navbar-inner">
@@ -13,51 +14,39 @@
           <!-- Nav items -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" href="{{ route('dashboard') }}">
-                <i class="ni ni-tv-2 text-primary"></i>
+              <a class="nav-link {{ Request::is('dashboard')? "text-primary" : "" }}" href="{{ route('dashboard') }}">
+                <i class="ni ni-app"></i>
                 <span class="nav-link-text">Halaman Utama</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href={{ route('kelas') }}>
-                <i class="ni ni-planet text-orange"></i>
+              <a class="nav-link {{ Request::is('kelas')? "text-primary" : "" }}" href={{ route('kelas') }}>
+                <i class="ni ni-bullet-list-67"></i>
                 <span class="nav-link-text">Daftar Kelas</span>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="examples/map.html">
-                <i class="ni ni-pin-3 text-primary"></i>
+                <i class="ni ni-circle-08"></i>
                 <span class="nav-link-text">Daftar Pengguna Baru</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('permission') }}">
-                <i class="ni ni-single-02 text-yellow"></i>
+              <a class="nav-link {{ Request::is('permission')? "text-primary" : "" }}" href="{{ route('permission') }}">
+                <i class="ni ni-single-02"></i>
                 <span class="nav-link-text">Daftar Izin Siswa</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('attendance') }}">
-                <i class="ni ni-bullet-list-67 text-default"></i>
+              <a class="nav-link {{ Request::is('attendance')? "text-primary" : "" }}" href="{{ route('attendance') }}">
+                <i class="ni ni-hat-3"></i>
                 <span class="nav-link-text">Daftar Kehadiran Siswa</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="examples/login.html">
-                <i class="ni ni-key-25 text-info"></i>
-                <span class="nav-link-text">Login</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="examples/register.html">
-                <i class="ni ni-circle-08 text-pink"></i>
-                <span class="nav-link-text">Register</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="examples/upgrade.html">
-                <i class="ni ni-send text-dark"></i>
-                <span class="nav-link-text">Upgrade</span>
+              <a class="nav-link" href="">
+                <i class="ni ni-single-copy-04"></i>
+                <span class="nav-link-text">Dokumentasi</span>
               </a>
             </li>
           </ul>
@@ -77,26 +66,14 @@
             </li>
             <li class="nav-item">
               <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
-                <i class="ni ni-palette"></i>
+                <i class="ni ni-square-pin"></i>
                 <span class="nav-link-text">Lokasi Sekolah</span>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html" target="_blank">
-                <i class="ni ni-ui-04"></i>
+                <i class="ni ni-hat-3"></i>
                 <span class="nav-link-text">LMS</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
-                <i class="ni ni-chart-pie-35"></i>
-                <span class="nav-link-text">Plugins</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active active-pro" href="examples/upgrade.html">
-                <i class="ni ni-send text-dark"></i>
-                <span class="nav-link-text">Upgrade to PRO</span>
               </a>
             </li>
           </ul>
